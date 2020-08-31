e52458c135f27cbc40bcc8548201b273a319dcd8
ayush8x8 <ayush8x8@gmail.com>
2020-08-31 20:32:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[800],
+      appBar: AppBar(
+        title: Center(child: Text('Am I Rich?')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    ),
+  ));
+}
